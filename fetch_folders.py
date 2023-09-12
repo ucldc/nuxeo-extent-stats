751f161a1d2b1c66ecd68f5afd373eb83607730e
Barbara Hui <barbara.hui@ucop.edu>
2023-09-12 13:33:08 -0700

    Modify fetch_folders.py to accept --all option

--- fetch_folders.py
+++ fetch_folders.py
@@ -1,6 +1,7 @@
 import sys, os
 import json
 import folderfetcher
+import argparse
 
 BUCKET = os.environ.get('S3_BUCKET')
 
@@ -18,15 +19,26 @@ campuses = [
     "UCSF",
 ]
 
-def main():
-    for campus in campuses:
+def main(params):
+    if params.all:
+        for campus in campuses:
+            fetch_folders(campus, params.depth)
+    elif params.campus:
+        fetch_folders(params.campus, params.depth)
 
-        # get folders for each campus
-        campus_basepath = f"/asset-library/{campus}"
+def fetch_folders(campus, depth):
+    campus_basepath = f"/asset-library/{campus}"
 
-        # fetch folders to a depth of 1
-        print(f"fetching folders for {campus_basepath}")
-        folderfetcher.fetch(campus_basepath, campus, 1)
+    # fetch folders to a depth of 1
+    print(f"fetching folders for {campus_basepath}")
+    folderfetcher.fetch(campus_basepath, campus, depth)
 
 if __name__ == "__main__":
-    sys.exit(main())
+    parser = argparse.ArgumentParser(description="create nuxeo extent stats report(s)")
+    top_folder = parser.add_mutually_exclusive_group(required=True)
+    top_folder.add_argument('--all', help="create reports for all campuses", action="store_true")
+    top_folder.add_argument('--campus', help="single campus")
+    parser.add_argument('--depth', help='max depth for folders, i.e. /asset-library/UCX/sub1/sub2', type=int, default=1)
+    args = parser.parse_args()
+
+    sys.exit(main(args))
