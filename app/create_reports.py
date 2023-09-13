f1f3c5110cd4eb7622ca96aa2931b0ad4074bfef
Barbara Hui <barbara.hui@ucop.edu>
2023-09-13 15:53:01 -0700

    Change script to run all tasks

--- app/create_reports.py
+++ app/create_reports.py
@@ -1,31 +1,27 @@
 import sys, os
 import argparse
-import extentreport
+
 import boto3
 
-def main(params):
-    if params.datasource == 'es' and params.es_api_broken:
-        query_db = True
-    else:
-        query_db = False
+import folderfetcher
+import metadatafetcher
+import extentreport
 
-    if params.all:
-        campuses = os.environ.get('CAMPUSES')
-        sub_prefixes = []
-        for campus in campuses:
-            sub_prefixes = get_child_prefixes(campus, params.datasource)
-            workbook_id = f"{campus}-{params.datasource}"
-            extentreport.report(workbook_id, campus, sub_prefixes, params.datasource, query_db)
-    elif params.campus:
-        sub_prefixes = get_child_prefixes(params.campus, params.datasource)
-        workbook_id = f"{params.campus}-{params.datasource}"
-        extentreport.report(workbook_id, params.campus, sub_prefixes, params.datasource, query_db)
-    elif params.path:
-        path = params.path.lstrip('/asset-library/')
-        sub_prefixes = get_child_prefixes(path, params.datasource)
-        campus_equiv = path.rstrip('/').replace('/', '_')
-        workbook_id = f"{campus_equiv}-{params.datasource}"
-        extentreport.report(workbook_id, campus_equiv, sub_prefixes, params.datasource, query_db)
+def fetch_metadata(folders, campus, datasource):
+    parent_uids = [folder['parent_uid'] for folder in folders]
+    for folder in folders:
+        next_page = {
+            "campus": campus,
+            "path": folder['path'],
+            "uid": folder['uid'],
+            "datasource": datasource
+        }
+
+        while next_page:
+            fetcher = metadatafetcher.Fetcher(next_page)
+            fetcher.fetch_page()
+            next_page = fetcher.next_page()
+
 
 def get_child_prefixes(folder, datasource):
     md_prefix = {
@@ -51,15 +47,40 @@ def get_child_prefixes(folder, datasource):
 
     return prefixes
 
+
+def main(params):
+    if params.datasource == 'es' and params.es_api_broken:
+        query_db = True
+    else:
+        query_db = False
+
+    if params.campus:
+        campuses = [params.campus]
+    elif params.all:
+        campuses = os.environ.get('CAMPUSES')
+
+    for campus in campuses:
+        print("**********************")
+        print(f"******   {campus}   ******")
+        print("**********************")
+
+        if not params.reportonly:
+            folders = folderfetcher.fetch(f"/asset-library/{campus}", campus, 1)
+            fetch_metadata(folders, campus, params.datasource)
+
+        workbook_id = f"{campus}-{params.datasource}"
+        prefixes = get_child_prefixes(campus, params.datasource)
+        extentreport.report(workbook_id, campus, prefixes, params.datasource, query_db)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="create nuxeo extent stats report(s)")
     top_folder = parser.add_mutually_exclusive_group(required=True)
     top_folder.add_argument('--all', help="create reports for all campuses", action="store_true")
     top_folder.add_argument('--campus', help="single campus")
-    top_folder.add_argument('--path', help="nuxeo path for folder")
     parser.add_argument('--derivatives', help="include derivatives in file count", default=True)
     parser.add_argument('--datasource', choices=['es', 'db'], help="metadata source: es (elasticsearch) or db (database)", default='es')
     parser.add_argument('--es_api_broken', action="store_true", help="set this option when the Nuxeo elasticsearch API is broken")
+    parser.add_argument('--reportonly', action="store_true", help="set this option when the metadata already exists on S3 and does not need to be fetched")
 
     args = parser.parse_args()
     sys.exit(main(args))
