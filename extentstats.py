4232e7c2f06286e10c2e39dbd5f1a565f3ac1a25
Barbara Hui <barbara.hui@ucop.edu>
2025-08-25 11:18:42 -0700

    Increase number of retries from 2 to 6

--- extentstats.py
+++ extentstats.py
@@ -39,7 +39,7 @@ def configure_http_session() -> requests.Session:
     http = requests.Session()
     retry_strategy = Retry(
         total=3,
-        backoff_factor=2,
+        backoff_factor=6,
         status_forcelist=[413, 429, 500, 502, 503, 504]
     )
     adapter = HTTPAdapter(max_retries=retry_strategy)
